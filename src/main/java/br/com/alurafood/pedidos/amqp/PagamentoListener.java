59da9b7989c0289ac28088b9ffa34393ce4c554b
Dani2003G <daniel0202almeida@outlook.com>
2025-09-11 22:02:09 -0300

    Tratamento de erro na fila: Adicao da validacao que vai dar o erro

--- src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -9,6 +9,14 @@ public class PagamentoListener {
 
     @RabbitListener(queues = "pagamentos.detalhes-pedido")
     public void recebeMensagem(PagamentoDto pagamento) {
+        System.out.println(pagamento.getId());
+        System.out.println(pagamento.getNumero());
+
+        if (pagamento.getNumero().equals("0000")) {
+            throw new RuntimeException("não consegui processar");
+        }
+
+
         String mensagem = """
                 Dados do pagamento: %s
                 Número do pedido: %s
